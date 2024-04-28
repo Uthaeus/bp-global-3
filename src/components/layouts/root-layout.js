f670a0b8cc3cdcb7169b7264f6f36d90128b69d4
Uthaeus <romanlavery@gmail.com>
2024-04-28 14:43:58 -0600

    created main footer and copyright components, imported to root layout

--- src/components/layouts/root-layout.js
+++ src/components/layouts/root-layout.js
@@ -1,9 +1,16 @@
 import { Outlet } from "react-router";
 
+import MainNavigation from "../navigation/main-navigation";
+import MainFooter from "../footer/main-footer";
+import Copyright from "../footer/copyright";
+
 function RootLayout() {
     return (
         <>
+            <MainNavigation />
             <Outlet />
+            <MainFooter />
+            <Copyright />
         </>
     );
 }
