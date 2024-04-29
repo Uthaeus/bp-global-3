819d5f93c48eb2aece1a90c3623ea55bb518e24b
Uthaeus <romanlavery@gmail.com>
2024-04-28 18:59:46 -0600

    built out user context using dummy data

--- src/store/user-context.js
+++ src/store/user-context.js
@@ -1,5 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 
+import { dummyAdmin } from "./dummy/dummy-users";
+
 export const UserContext = createContext({
     user: null,
     isAdmin: false,
@@ -7,3 +9,28 @@ export const UserContext = createContext({
     setIsAdmin: () => {},
     logOutUser: () => {}
 });
+
+export default UserContextProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [isAdmin, setIsAdmin] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setUser(dummyAdmin);
+        setIsAdmin(true);
+        setIsLoading(false);
+    }, []);
+
+    const logOutUser = () => {
+        setUser(null);
+        setIsAdmin(false);
+    }
+
+    const value = {
+        user,
+        isAdmin,
+        logOutUser
+    }
+
+    return <UserContext.Provider value={value}>{!isLoading && children}</UserContext.Provider>;
+}
