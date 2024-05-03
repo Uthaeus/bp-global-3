3a94fa114f18476c2412e94b4204bfa200771fd5
Uthaeus <romanlavery@gmail.com>
2024-05-03 12:58:23 -0600

    built out account edit form

--- src/components/account/edit-account.js
+++ src/components/account/edit-account.js
@@ -22,15 +22,63 @@ function EditAccount() {
 
     if (isLoading) {
         return (
-            <div className="edit-account">
+            <div className="auth">
                 <h1>Loading...</h1>
             </div>
         );
     }
 
     return (
-        <div className="edit-account">
-            <h1>Edit Account</h1>
+        <div className="auth">
+            <h1 className="auth-title">Edit Account</h1>
+
+            <form className="auth-form mb-3" onSubmit={handleSubmit(submitHandler)}>
+                <div className="form-group">
+                    <label htmlFor="name">Name</label>
+                    <input
+                        type="text"
+                        id="name"
+                        className="form-control"
+                        autoFocus={true}
+                        {...register("name")}
+                    />
+                    {errors.name && <span className="text-danger">This field is required</span>}
+                </div>
+
+                <div className="form-group mb-3">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        className="form-control"
+                        {...register("email")}
+                    />
+                </div>
+
+                <div className="form-group mb-3">
+                    <label htmlFor="password">New Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        placeholder="Leave blank to keep the same"
+                        className="form-control"
+                        {...register("password")}
+                    />
+                </div>
+
+                <div className="form-group mb-4">
+                    <label htmlFor="confirmPassword">Confirm Password</label>
+                    <input
+                        type="password"
+                        id="confirmPassword"
+                        placeholder="Only need to confirm if changing password"
+                        className="form-control"
+                        {...register("confirmPassword")}
+                    />
+                </div>
+
+                <button type="submit" className="btn btn-primary">Update</button>
+            </form>
         </div>
     );
 }
