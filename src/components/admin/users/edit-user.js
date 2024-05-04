949318c0e9fb128eed9f798f074e6f670953eb64
Uthaeus <romanlavery@gmail.com>
2024-05-04 15:02:37 -0600

    built deleteHandler for users, successfully removing orders

--- src/components/admin/users/edit-user.js
+++ src/components/admin/users/edit-user.js
@@ -1,24 +1,38 @@
 import { useContext, useState, useEffect } from "react";
-import { useParams } from "react-router";
+import { useParams, useNavigate } from "react-router";
 
 import { UsersContext } from "../../../store/users-context";
 
 import UserForm from "./user-form";
 
 function EditUser() {
-    const { users } = useContext(UsersContext);
+    const { users, deleteUser } = useContext(UsersContext);
     const { id } = useParams();
     const [user, setUser] = useState(null);
+    const navigate = useNavigate();
 
     useEffect(() => {
         setUser(users.find((user) => +user.id === +id));
     }, [id, users]);
+
+    const deleteHandler = () => {
+        console.log("delete");
+
+        deleteUser(user.id);
+
+        navigate("/admin/users");
+    }  
 
     return (
         <div className="admin-user">
             <h1 className="admin-user-title">Edit User</h1>
 
             <UserForm user={user} />
+
+            <div className="admin-user-actions mt-5">
+                <button className="btn btn-success mx-2" onClick={() => navigate(`/admin/users/${user?.id}`)}>Back</button>
+                <button className="btn btn-danger mx-2" onClick={deleteHandler}>Delete</button>
+            </div>
         </div>
     );
 }
